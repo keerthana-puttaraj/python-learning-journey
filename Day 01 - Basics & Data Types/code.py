e7dc7424199f72535bc71f27f1448d3fef5bf974
KEERTHANA S P <keerthanaputtaraj@gmail.com>
2026-04-17 19:45:30 +0530

    Added Day 01 Python code

---
+++ Day 01 - Basics & Data Types/code.py
@@ -0,0 +1,35 @@
+# Day 1: Basics & Data Types
+
+# Data Types
+a = 10
+b = 3.5
+c = True
+d = 2 + 3j
+name = "Keerthana"
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(name))
+
+
+# Type Casting
+x = "20"
+print(int(x) + 5)
+
+
+# String Concatenation
+print("Hello " + "Python")
+
+
+# Keywords
+import keyword
+print(keyword.kwlist)
+
+
+# Indentation
+if a > 5:
+    print("a is greater")
+else:
+    print("a is small")

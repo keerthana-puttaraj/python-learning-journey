87eca61e0c56c95e9f1de80ff459c473e9c2cb90
KEERTHANA S P <keerthanaputtaraj@gmail.com>
2026-04-18 20:16:39 +0530

    Added Day 02 Python code

---
+++ Day 02 - Data Structures & Operators/day2.py
@@ -0,0 +1,61 @@
+# Day 2: Data Structures & Operators
+
+# Data Structures
+numbers = [1, 2, 3]
+t = (10, 20)
+student = {"name": "Keerthana"}
+s = {1, 2, 2, 3}
+
+print(numbers)
+print(t)
+print(student)
+print(s)
+
+
+# Slicing
+print(numbers[0:2])
+print("Python"[0:3])
+
+
+# Arithmetic Operators
+a = 10
+b = 5
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+
+# Comparison Operators
+print(a > b)
+print(a == b)
+print(a != b)
+
+
+# Logical Operators
+print(a > 5 and b < 10)
+print(a > 5 or b > 10)
+
+
+# Assignment Operators
+x = 5
+x += 2
+print(x)
+
+
+# Membership Operators
+print(2 in numbers)
+print(10 not in numbers)
+
+
+# Identity Operators
+x = [1, 2]
+y = x
+z = [1, 2]
+print(x is y)
+print(x is z)
+
+
+# Bitwise Operators
+print(5 & 3)
+print(5 | 3)
